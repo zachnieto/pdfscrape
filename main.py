f7c443fe73e4bc10f04c8a8367a20f5537166ca8
zachnieto <nieto.z@husky.neu.edu>
2022-07-25 19:03:50 -0400

    regex matching

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
+import re
+
+import xlwings as xw
 from PyPDF2 import PdfReader
 
 
 class Scrape:
+    MAX_DISTANCE = 20  # chars
 
     def __init__(self, pdf):
-        self.keywords = open("keywords.txt", "r").read().split("\n")
+        self.wb = xw.Book(r"keywords.xlsx")
+        self.sheet = self.wb.sheets[0]
+        self.keywords = [kwarg for kwarg in self.sheet.range("A:A")[1:500].value if kwarg]
 
-        reader = PdfReader(pdf)
-        self.pages = [page.extract_text().split() for page in reader.pages]
-        self.content = [word for page in self.pages for word in page]
-        # self.content = "Test is the value 4. 7 matches to Hello. Word is not with Test but with 1. Test. 3 is Dog".split()
+        print(f"Keywords: {', '.join(self.keywords)}")
 
-        matching = None
+        self.pages = [page.extract_text() for page in PdfReader(pdf).pages]
+        self.content = '\n'.join(self.pages)
+        self.split_content = self.content.split()
 
-        for word in self.content:
-            word = word.replace('.', '', 1).replace(",", '').replace("$", "")
-            if word in self.keywords:
-                if matching and self.word_is_value(matching):
-                    self.log_match(word, matching)
-                    matching = None
-                else:
-                    matching = word
-            elif self.word_is_value(word):
-                if matching and matching in self.keywords:
-                    self.log_match(matching, word)
-                    matching = None
-                else:
-                    matching = word
+        matches = []
 
-            if matching and matching[-1] == ".":
-                matching = None
+        r = re.finditer("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", self.content)
+        for match in r:
+            matches.append((match.group(), match.start(0)))
+
+        r = re.finditer("|".join(self.keywords), self.content)
+        for match in r:
+            matches.append((match.group(), match.start(0)))
+
+        matches = sorted(matches, key=lambda x: x[1])
+
+        for index, match in enumerate(matches):
+            if match[0] not in self.keywords:
+                continue
+
+            prev_val = matches[index - 1] if matches[index - 1][0] not in self.keywords else (None, float("inf"))
+            next_val = matches[index + 1] if matches[index + 1][0] not in self.keywords else (None, float("inf"))
+
+            if (dist := abs(prev_val[1] - match[1])) < abs(next_val[1] - match[1]) and dist < Scrape.MAX_DISTANCE:
+                print(f"{match[0]} - {prev_val[0]}")
+            elif abs(prev_val[1] - match[1]) > (dist := abs(next_val[1] - match[1])) and dist < Scrape.MAX_DISTANCE:
+                print(f"{match[0]} - {next_val[0]}")
 
     @staticmethod
     def log_match(key, value):
@@ -41,4 +52,4 @@ class Scrape:
 
 
 if __name__ == "__main__":
-    Scrape("test.pdf")
+    Scrape("term-loan-credit-agreement.pdf")
